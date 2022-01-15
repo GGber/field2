3f1d6c268ac4e6ebedcd024d8b05a09cd9bb2e1b
GGber <351661343@qq.com>
2022-01-15 20:10:44 +0800

    MSVC创建工程

--- field2/field2.cpp
+++ field2/field2.cpp
@@ -1,7 +1,13 @@
 #include "field2.h"
+#include "QDebug.h"
 
 field2::field2(QWidget *parent)
     : QMainWindow(parent)
 {
+	#if _MSC_VER >= 1600                        //ÖĞÎÄÂÒÂë
+	#pragma execution_character_set("utf-8")
+	#endif
+
     ui.setupUi(this);
+	qDebug() << "Éµ±ÆÍäÍä" << endl;
 }
